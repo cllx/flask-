fac382a2b88aefd71b8f21b4078a86391ac1617f
tea&coffee <18981651893@163.com>
2019-08-14 16:05:32 +0800

    pca更新

--- pca.py
+++ pca.py
@@ -12,13 +12,15 @@ def loadData(str):
     sArr = [line.strip().split("\t") for line in fr.readlines()]
     datArr = [[float(s) for s in line[1][1:-1].split(", ")] for line in sArr]
     matA = mat(datArr)
-    print(matA.shape)
+    #print(matA.shape)
     nameArr = [line[0] for line in sArr]
     return matA, nameArr
 
 def pca(inputM, k):
     covM = cov(inputM, rowvar=0)
     s, V = linalg.eig(covM)
+    s = s.real
+    V = V.real
     paixu = argsort(s)
     paixuk = paixu[:-(k+1):-1]
     kwei = V[:,paixuk]
@@ -29,7 +31,6 @@ def pca(inputM, k):
 def plotV(a, labels, filename):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    print("aaa")
     font = { 'fontname':'Tahoma', 'fontsize':8, 'verticalalignment': 'top', 'horizontalalignment':'center' }
     ax.scatter(a[:,0].tolist(), a[:,1].tolist(), marker = ' ')
     ax.set_xlim(-0.8,0.8)
@@ -38,7 +39,7 @@ def plotV(a, labels, filename):
     for label, x, y in zip(labels, a[:, 0], a[:, 1]):
         i += 1
         s = random.uniform(0,100)
-        if i<14951:
+        if i<80:
             if s > 3.1:
                 continue
         else:
